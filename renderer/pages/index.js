7214e33f09e90c5d340488353735ce0dfae46bc5
Nghia Nguyen <41444405+NghiaNguyenTT@users.noreply.github.com>
2021-07-06 22:07:38 +0700

    Initialized Version

---
+++ renderer/pages/index.js
@@ -0,0 +1,12 @@
+import NextHead from "next/head";
+import Layout from "../comps/Layout";
+
+export default function Main() {
+  return (
+    <Layout>
+      <NextHead>
+        <title>Main</title>
+      </NextHead>
+    </Layout>
+  );
+}
